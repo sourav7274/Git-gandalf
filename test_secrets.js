aa78b0a5230841b17ef17e58caaeb1dc62a9dd20
Sourav <samjhonson932@gmail.com>
2026-01-11 20:30:05 +0530

    cas test

--- test_secrets.js
+++ test_secrets.js
@@ -1,7 +1,7 @@
 // This file contains fake secrets for testing the pre-commit hook
 
-const AWS_ACCESS_KEY_ID = "AKIAiosFODNN7EXAMPLE";
-const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const AWS_ACCESS_KEY_ID = "AKIAiosFODNN7EXAdfgdfgMPLE";
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEYgfdg";
 
 const STRIPE_API_KEY = "sk_live_51Hc7AxKZ0...0000";
 
