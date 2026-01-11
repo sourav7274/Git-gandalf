7a4bac198a986209c79a3044ed698959cd6738bb
Sourav <samjhonson932@gmail.com>
2026-01-11 21:32:08 +0530

    test: add blank line to test file

--- test_secrets.js
+++ test_secrets.js
@@ -1,6 +1,5 @@
 // This file contains fake secrets for testing the pre-commit hook
 
 
-const STRIPE_API_KEY = "sk_live_51Hc7AxKZ0...0000";
 
 console.log("Connecting to AWS with key:", AWS_ACCESS_KEY_ID);
